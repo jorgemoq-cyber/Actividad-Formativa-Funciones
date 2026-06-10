f4d098fe8c84eb2799e4e50a00537bcb239c43e0
jorgemoq-cyber <jo.moragaq@duocuc.cl>
2026-06-09 21:49:15 -0400

    Cambios datos ingresados por el usuario y If para ganar
    Añadí un While True, junto a una serie de validaciones para que el usuario ingrese datos correctos para jugar, también se añadió el Try para evitar ingreso de strings.
    
    Se cambio el requisito para ganar de tener todos los números perfectos en el mismo orden a solo tener los 7 números en cualquier orden.

--- casino.py
+++ casino.py
@@ -3,10 +3,18 @@ nums_jugador = []
 rondas = []
 
 for i in range(7):
-    ingreso_jugador = int(input("Ingrese sus numeros de la suerte: "))
-    nums_jugador.append(ingreso_jugador)
-    print(f"Usted ingreso los siguientes numeros: {nums_jugador}")
+    while True:
+        try:
+            ingreso_jugador = int(input("Ingrese sus numeros de la suerte: "))
+            if 1 <= ingreso_jugador <= 49:
+                nums_jugador.append(ingreso_jugador)
+                break
+            else:
+                print("Error! El numero ingresado debe ser un numero entero positivo entre 1 y 49")
+        except(ValueError):
+            print("Solo puede ingresar numeros enteros positivos")
 
+print(f"Usted ingreso los siguientes numeros: {nums_jugador}")
 
 while len(rondas) < 3:
     nums_rondas = [random.randint(1, 49) for _ in range(7)]
@@ -18,7 +26,7 @@ for e in range(3):
     for u in range (7):
         print(rondas[e][u])
     
-    if nums_jugador == rondas[e]:
+    if sorted(nums_jugador) == sorted(rondas[e]):
         ganaste = True
         print("Felicidades has acertado los numeros del gran premio!")
         break
